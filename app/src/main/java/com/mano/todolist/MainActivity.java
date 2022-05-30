c000e62544ea7f85dafcd637bd16b845ece3e726
Mahnoor <mahnoorali57se@gmail.com>
2022-05-31 01:20:43 +0500

    adapter updated

--- app/src/main/java/com/mano/todolist/MainActivity.java
+++ app/src/main/java/com/mano/todolist/MainActivity.java
@@ -13,31 +13,28 @@ import java.sql.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView myListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView myListView = findViewById(R.id.todo);
+        myListView = findViewById(R.id.todo);
         ArrayList<String> toDo = new ArrayList<>();
         toDo.add("quiz");
         toDo.add("rest");
         toDo.add("workout");
         toDo.add("cooking");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>();
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>()
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, grocery);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDo);
         myListView.setAdapter(arrayAdapter);
 
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
                 String text = "Item" + position+ " " + ((TextView) view).getText().toString();
                 Toast.makeText(MainActivity.this,text , Toast.LENGTH_SHORT).show();
-
             }
         });
     }
