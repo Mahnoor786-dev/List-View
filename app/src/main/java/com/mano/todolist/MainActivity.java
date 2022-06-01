6837b57d2fbc12d3e285e5d50a35842f60f8dda1
Mahnoor <mahnoorali57se@gmail.com>
2022-06-01 23:30:51 +0500

    ToDo list updated

--- app/src/main/java/com/mano/todolist/MainActivity.java
+++ app/src/main/java/com/mano/todolist/MainActivity.java
@@ -5,28 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     ListView myListView;
+    Button addBtn;
+    EditText task;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        addBtn = findViewById(R.id.add);
+        task = findViewById(R.id.task);
         myListView = findViewById(R.id.todo);
-        ArrayList<String> toDo = new ArrayList<>();
-        toDo.add("quiz");
-        toDo.add("rest");
-        toDo.add("workout");
-        toDo.add("cooking");
+        ArrayList<String> friendList = new ArrayList<String>();
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDo);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendList);
         myListView.setAdapter(arrayAdapter);
 
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -37,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,text , Toast.LENGTH_SHORT).show();
             }
         });
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                friendList.add(task.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+                Collections.sort(friendList);
+                task.setText("");
+            }});
+
     }
 }
-
