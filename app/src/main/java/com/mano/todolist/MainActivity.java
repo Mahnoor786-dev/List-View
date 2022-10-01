672b6a42bdfc189a2159c9476937a62348e5db43
Mahnoor <mahnoorali57se@gmail.com>
2022-10-01 20:17:38 +0500

    last

--- app/src/main/java/com/mano/todolist/MainActivity.java
+++ app/src/main/java/com/mano/todolist/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mano.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,8 +29,6 @@ public class MainActivity extends AppCompatActivity {
         task = findViewById(R.id.task);
         myListView = findViewById(R.id.todo);
         ArrayList<String> friendList = new ArrayList<String>();
-
-
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendList);
         myListView.setAdapter(arrayAdapter);
 
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
-                String text = "Item" + position+ " " + ((TextView) view).getText().toString();
+                String text = "Task #" + position+ ": " + ((TextView) view).getText().toString();
                 Toast.makeText(MainActivity.this,text , Toast.LENGTH_SHORT).show();
             }
         });
@@ -49,6 +50,33 @@ public class MainActivity extends AppCompatActivity {
                 Collections.sort(friendList);
                 task.setText("");
             }});
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Delete Recod")
+                        .setMessage("Do you really want to deete this task!")
+                        .setIcon(R.drawable.ic_launcher_background)
+                        .setCancelable(false)
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                friendList.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }});
+
+
+
 
     }
 }
